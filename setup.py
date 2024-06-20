8ecaa88f150f950ae367099d5f6f908c68c62a45
arakkk <bdkouiti5013@gmail.com>
2024-06-21 08:16:14 +0900

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="textilels",
     version="0.1.0",
-    install_requres=["pygls"],
+    install_requres=["pygls", "lsprotocol"],
     # packages=["module"],
     entry_points={"console_scripts": ["textilels=module.textilels:main"]},
 )
